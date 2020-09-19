11e83dae8d64dcf3ba5dc1a8be702b19a7e863ac
Sean Steel <seanrsteel@gmail.com>
2020-09-19 09:48:53 -0600

    Create instance of test for offset class

---
+++ lib/offset.rb
@@ -0,0 +1,2 @@
+require_relative 'key'
+require 'pry'
---
+++ test/offset_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+require './lib/offset'
+require 'pry'
+
+class OffsetTest < Minitest::Test
+
+  def test_it_exists
+    offset = Offset.new
+
+    assert_instance_of Offset, offset
+  end
+end
