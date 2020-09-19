dfc4766bd9ea9954d137f7e7a735b20dc2832a34
Sean Steel <seanrsteel@gmail.com>
2020-09-19 09:00:36 -0600

    Add test for splitting up keys

--- test/key_test.rb
+++ test/key_test.rb
@@ -12,7 +12,18 @@ class KeyTest < Minitest::Test
 
   def test_random_five_digits
     key = Key.new
-    key.stubs(:random_five_digits).returns('Random digits')
-    assert_equal 'Random digits', key.random_five_digits
+    key.stubs(:random_five_digits).returns([1, 2, 3, 4, 5])
+    assert_equal [1, 2, 3, 4, 5], key.random_five_digits
   end
+
+  def test_split_up_keys
+    key = Key.new
+
+    expected = {:a => 12,
+                :b => 65,
+                :c => 21,
+                :d => 01}
+
+    assert_equal expected, key.split_up_keys
+  end 
 end
