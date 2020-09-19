364602dde66cfdb10d96eba68d955601ea13ec8f
Sean Steel <seanrsteel@gmail.com>
2020-09-19 11:39:42 -0600

    Change shift initialize to take in all strings

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,15 +3,19 @@ require_relative 'offset'
 require 'pry'
 
 class Shift
-  attr_reader :key, :offset
-  def initialize
-    @key = Key.new.hash_of_keys
-    @offset = Offset.new.offset_hash
+  attr_reader :string, :key, :date
+  def initialize(string,
+                 key = Key.new.hash_of_keys,
+                 date = Offset.new.offset_hash)
+    @string = string
+    @key = key
+    @date = date
+    # @characters =
   end
 
-  def total_shift_amount
-    @key.merge(@offset) do |letter, key, offset|
-      key + offset
-    end
-  end
+  # def total_shift_amount
+  #   @key.merge(@offset) do |letter, key, offset|
+  #     key + offset
+  #   end
+  # end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,48 +5,51 @@ require 'pry'
 class ShiftTest < Minitest::Test
 
   def test_it_exists
-    shift = Shift.new
+    shift = Shift.new('Hello World', '02715', '040895')
 
     assert_instance_of Shift, shift
+    assert_equal 'Hello World', shift.string
+    assert_equal '02715', shift.key
+    assert_equal '040895', shift.date
   end
 
-  def test_key
-    shift = Shift.new
-    mock_expected = {:a => 1,
-                     :b => 2,
-                     :c => 3,
-                     :d => 4}
-    shift.stubs(:key).returns(mock_expected)
-    expected = {:a => 1,
-                :b => 2,
-                :c => 3,
-                :d => 4}
-
-    assert_equal expected, shift.key
-  end
-
-  def test_offset
-    shift = Shift.new
-    expected = {:a => 6,
-                :b => 4,
-                :c => 0,
-                :d => 0}
-
-    assert_equal expected, shift.offset
-  end
-
-  def test_total_shift_amount
-    shift = Shift.new
-    mock_expected = {:a => 1,
-                     :b => 2,
-                     :c => 3,
-                     :d => 4}
-    shift.stubs(:total_shift_amount).returns(mock_expected)
-    expected = {:a => 1,
-                :b => 2,
-                :c => 3,
-                :d => 4}
-
-    assert_equal expected, shift.total_shift_amount
-  end
+  # def test_key
+  #   shift = Shift.new
+  #   mock_expected = {:a => 1,
+  #                    :b => 2,
+  #                    :c => 3,
+  #                    :d => 4}
+  #   shift.stubs(:key).returns(mock_expected)
+  #   expected = {:a => 1,
+  #               :b => 2,
+  #               :c => 3,
+  #               :d => 4}
+  #
+  #   assert_equal expected, shift.key
+  # end
+  #
+  # def test_offset
+  #   shift = Shift.new
+  #   expected = {:a => 6,
+  #               :b => 4,
+  #               :c => 0,
+  #               :d => 0}
+  #
+  #   assert_equal expected, shift.offset
+  # end
+  #
+  # def test_total_shift_amount
+  #   shift = Shift.new
+  #   mock_expected = {:a => 1,
+  #                    :b => 2,
+  #                    :c => 3,
+  #                    :d => 4}
+  #   shift.stubs(:total_shift_amount).returns(mock_expected)
+  #   expected = {:a => 1,
+  #               :b => 2,
+  #               :c => 3,
+  #               :d => 4}
+  #
+  #   assert_equal expected, shift.total_shift_amount
+  # end
 end
