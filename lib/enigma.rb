9de5ee284d468068d32226455c2003685076107f
Sean Steel <seanrsteel@gmail.com>
2020-09-19 16:18:28 -0600

    Add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,17 @@ require 'pry'
 
 class Enigma
 
+  def initialize
+    @shift = Shift.new('hello world', '02715', '040895')
+    @encryption = Hash.new
+  end
 
+  def encrypt(message, key, date)
+    @encryption[:encryption] = @shift.shift_message
+    @encryption[:key] = key
+    @encryption[:date] = date
+    @encryption
+  end
   # def encrypt(message, key = key_method, date = date_method)
   # end
   #
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,9 +13,9 @@ class EnigmaTest < Minitest::Test
   def test_encrypt
     enigma = Enigma.new
 
-    expected = {:encryption => '',
+    expected = {:encryption => 'keder ohulw',
                 :key => '02715',
                 :date => '040895'}
-    assert_equal expected, enigma.encrypt('Hello World', '02715', '040895')
+    assert_equal expected, enigma.encrypt('hello world', '02715', '040895')
   end
 end
