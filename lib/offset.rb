2415e3c605392d1a3b8af2712de6b4cadf85bca5
Sean Steel <seanrsteel@gmail.com>
2020-09-19 10:44:04 -0600

    Create shift class and test it exists

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,4 +1,3 @@
-require_relative 'key'
 require 'pry'
 
 class Offset
@@ -16,9 +15,9 @@ class Offset
   end
 
   def offset_hash
-    key_hash = {a: offset_digits_from_date[0].to_i,
-                b: offset_digits_from_date[1].to_i,
-                c: offset_digits_from_date[2].to_i,
-                d: offset_digits_from_date[3].to_i}
+    offset_hash = {a: offset_digits_from_date[0].to_i,
+                   b: offset_digits_from_date[1].to_i,
+                   c: offset_digits_from_date[2].to_i,
+                   d: offset_digits_from_date[3].to_i}
   end
 end
