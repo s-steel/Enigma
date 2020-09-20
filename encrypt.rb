12917ea805b6bf33cf7b0454f17ecb5537220857
Sean Steel <seanrsteel@gmail.com>
2020-09-20 11:25:16 -0600

    Create command line interface for encryption

--- encrypt.rb
+++ encrypt.rb
@@ -1,10 +1,23 @@
 require './lib/enigma'
-
 enigma = Enigma.new
 
-enigma.encrypt('hello world', '02715', '040895')
 
-puts "Created 'encrypted.txt' with the key #{enigma.encryption[:key]} and date #{enigma.encryption[:date]}"
+read_file = File.open(ARGV[0], 'r')
+
+incoming_message = read_file.read
+
+read_file.close
+
+encrypted_message = enigma.encrypt(incoming_message, '02715', '040895')
+
+write_encryption = File.open(ARGV[1], 'w')
+
+write_encryption.write(encrypted_message[:encryption])
+
+write_encryption.close
+
+
+puts "Created '#{ARGV[1]}' with the key #{enigma.encryption[:key]} and date #{enigma.encryption[:date]}"
 
 # $ ruby ./lib/encrypt.rb message.txt encrypted.txt
 # Created 'encrypted.txt' with the key 82648 and date 240818
