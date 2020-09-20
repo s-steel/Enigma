b9836a6d21b88bb27aaa273700c7bb8730af7817
Sean Steel <seanrsteel@gmail.com>
2020-09-20 10:41:45 -0600

    Edit decrypt file for only decryption

--- decrypt.rb
+++ decrypt.rb
@@ -1,10 +1,10 @@
 require './lib/enigma'
 
-# enigma.Enigma.new
-#
-# enigma.encrypt('hello world', '02715', '040895')
-# enigma.decrypt('keder ohulw', '02715', '040895')
+enigma = Enigma.new
 
+enigma.decrypt('keder ohulw', '02715', '040895')
+
+puts "Created 'decrypted.txt' with the key #{enigma.decryption[:key]} and date #{enigma.decryption[:date]}"
 # $ ruby ./lib/encrypt.rb message.txt encrypted.txt
 # Created 'encrypted.txt' with the key 82648 and date 240818
 # $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
