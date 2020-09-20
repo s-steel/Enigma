8b0db6877e720deda2ac55106fb84a92ba8708bf
Sean Steel <seanrsteel@gmail.com>
2020-09-20 10:25:50 -0600

    Create runner file

---
+++ decrypt.rb
@@ -0,0 +1,11 @@
+require './lib/enigma'
+
+# enigma.Enigma.new
+#
+# enigma.encrypt('hello world', '02715', '040895')
+# enigma.decrypt('keder ohulw', '02715', '040895')
+
+# $ ruby ./lib/encrypt.rb message.txt encrypted.txt
+# Created 'encrypted.txt' with the key 82648 and date 240818
+# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
+# Created 'decrypted.txt' with the key 82648 and date 240818
