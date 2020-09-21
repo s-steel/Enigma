4fa7debe07a66395a80b11cfed8c32819301423c
Sean Steel <seanrsteel@gmail.com>
2020-09-21 14:38:50 -0600

    Shorted character variable

--- lib/shift.rb
+++ lib/shift.rb
@@ -10,9 +10,7 @@ class Shift
     @message = message
     @key = key
     @date = date
-    @characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                   'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    @characters = ("a".."z").to_a << " "
   end
 
   def total_shift_amount
